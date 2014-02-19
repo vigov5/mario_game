38c924fa43aaa32d2e69d19a51fe8bd970cf35b4
vigov5 <nguyenanhtien2210@gmail.com>
2014-02-19 23:29:52 +0700

    refs#6 mario - add base code and property

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import sys
+
 import pygame
+
+import mario
+import config
 
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
@@ -18,34 +22,40 @@ class Game():
         self.screen = pygame.display.set_mode(self.size)
         self.clock = self.pygame.time.Clock()
         # TODO: init sprite, tile,...
+        self.mario_sprite = mario.Mario()
 
     def run(self):
         # main game loop
         while True:
             # hold frame rate at 60 fps
-            self.clock.tick(60)
+            self.clock.tick(1000)
             # enumerate event
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit(0)
                 # sprite handle event
-                self.update(event)
+                self.handle(event)
 
+            self.update()
             # re-draw screen
             self.draw(self.screen)
 
     def draw(self, screen):
+        screen.fill(config.WHITE)
         if pygame.font:
             font = pygame.font.Font(None, 36)
             text = font.render("Hello World !", 1, (255, 0, 0))
             textpos = text.get_rect(centerx=self.width/2)
             self.screen.blit(text, textpos)
         # TODO: sprite draw
+        self.mario_sprite.draw(self.screen)
         self.pygame.display.flip()
 
+    def update(self):
+        self.mario_sprite.update()
 
-    def update(self, event):
-        pass
+    def handle(self, event):
+        self.mario_sprite.handle(event)
 
 if __name__ == '__main__':
     g = Game()
